2b01cea914e52b9701734fbbb2fcdd688477758f
Jay Moriarty <43649913+moriarty3k@users.noreply.github.com>
2022-07-06 02:26:18 +0700

    Delete project directory

--- project/main.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "main.h"
-
-int main(int argc, char** argv) {
-	return italy(argc, argv);
-
-	return 0;
-}
-
--- project/main.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int italy(int argc, char** argv);
-#endif // !MAIN_H
